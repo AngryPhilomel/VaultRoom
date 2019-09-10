09c8825e8e370ec0d12d28149447fa6fcb1fde22
AngryPhilomel <an.aries@icloud.com>
2019-09-10 19:27:23 +0300

    extra=0

--- mainPyVR/vaultroom/forms.py
+++ mainPyVR/vaultroom/forms.py
@@ -3,4 +3,4 @@ from django.forms import modelformset_factory
 from .models import Stock, Storages, Products
 
 StockKorrSet = modelformset_factory(Stock, fields=('storage', 'product', 'quantity'),
-								   can_delete=True)
+								   can_delete=True, extra=0)
--- mainPyVR/vaultroom/views.py
+++ mainPyVR/vaultroom/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.http import HttpResponse
 
 from .models import Stock, Storages, Products, Done
 from .forms import StockKorrSet
+
+
 
 def index(request):
     stc = Stock.objects.all()
     storages = Storages.objects.all()
     context = {'stc': stc, 'storages': storages}
     return render(request, 'vaultroom/index.html', context)
+
+
 
 def by_storage(request, storage_id):
     stc = Stock.objects.filter(storage=storage_id)
@@ -18,6 +21,8 @@ def by_storage(request, storage_id):
     context = {'stc': stc, 'current_storage': current_storage, 'storages': storages,}
     return render(request, 'vaultroom/by_storage.html', context)
 
+
+
 def by_product(request, product_id):
     stc = Stock.objects.filter(product=product_id)
     storages = Storages.objects.all()
@@ -25,10 +30,13 @@ def by_product(request, product_id):
     context = {'stc': stc, 'current_product': current_product, 'storages': storages,}
     return render(request, 'vaultroom/by_product.html', context)
 
+
+
 def done(request):
     done = Done.objects.order_by('-time')
     context = {'done': done}
     return render(request, 'vaultroom/done.html', context)
+
 
 def stockKorr(request, product_id):
     if request.method == 'POST':
